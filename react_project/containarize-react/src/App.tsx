5c551e0db565e113b4a24f9bb4cd964600150f5f
alisy400 <horizonplus1411@gmail.com>
2025-08-24 14:39:20 +0530

    Fix react_project/containarize-react: convert from submodule to regular folder
    - This is the final gitlink/submodule entry that needed to be fixed
    - All folders are now properly tracked as regular directories
    - No more missing files in the docker-everything repository

---
+++ react_project/containarize-react/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I have edited this code so now fuck off!!
+          really nigga, I am gonna fuck you mom tonight
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
